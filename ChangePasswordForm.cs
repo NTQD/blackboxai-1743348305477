a8c1190af5e525a3e371efa9fd42a7fba286a970
VuLN <132083750+NTQD@users.noreply.github.com>
2025-03-31 00:20:43 +0700

    Add ChangePasswordForm.cs 1743355261537

--- ChangePasswordForm.cs
+++ ChangePasswordForm.cs
@@ -1,36 +1,53 @@
 using System;
 using System.Windows.Forms;
 
-public partial class ChangePasswordForm : Form
+namespace FinanceApp
 {
-    public ChangePasswordForm()
+    public partial class ChangePasswordForm : Form
     {
-        InitializeComponent();
-    }
-
-    private void btnChangePassword_Click(object sender, EventArgs e)
-    {
-        if (txtNewPassword.Text != txtConfirmPassword.Text)
+        public ChangePasswordForm()
         {
-            MessageBox.Show("Mật khẩu mới không khớp!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            return;
+            InitializeComponent();
         }
 
-        if (DatabaseHelper.ChangePassword(CurrentUser.UserId, txtCurrentPassword.Text, txtNewPassword.Text))
+        private void btnSave_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Đổi mật khẩu thành công!", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            this.DialogResult = DialogResult.OK;
+            try
+            {
+                if (txtNewPassword.Text != txtConfirmPassword.Text)
+                {
+                    MessageBox.Show("Mật khẩu mới và xác nhận không khớp!", "Lỗi",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (Helpers.DatabaseHelperExtensions.ChangePassword(
+                    CurrentUser.UserId,
+                    txtCurrentPassword.Text,
+                    txtNewPassword.Text))
+                {
+                    MessageBox.Show("Đổi mật khẩu thành công!", "Thành công",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.DialogResult = DialogResult.OK;
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Mật khẩu hiện tại không đúng!", "Lỗi",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi đổi mật khẩu: {ex.Message}", "Lỗi",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
             this.Close();
         }
-        else
-        {
-            MessageBox.Show("Mật khẩu hiện tại không đúng hoặc có lỗi xảy ra!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
-        }
-    }
-
-    private void btnCancel_Click(object sender, EventArgs e)
-    {
-        this.DialogResult = DialogResult.Cancel;
-        this.Close();
     }
 }
