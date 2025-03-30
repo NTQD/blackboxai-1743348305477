18ec6a31684a692aaab5bbd1d48ed59867f2329f
VuLN <132083750+NTQD@users.noreply.github.com>
2025-03-30 22:58:17 +0700

    Add Models/Transaction.cs 1743350315378

---
+++ Models/Transaction.cs
@@ -0,0 +1,14 @@
+namespace FinanceApp.Models
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+        public int UserId { get; set; }
+        public int? IncomeCategoryId { get; set; }
+        public int? ExpenseCategoryId { get; set; }
+        public int WalletId { get; set; }
+        public decimal Amount { get; set; }
+        public string? Description { get; set; }
+        public DateTime TransactionDate { get; set
+    }
+}
