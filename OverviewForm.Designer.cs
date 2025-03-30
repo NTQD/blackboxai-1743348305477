20515022f5a704341f1c9e89b84b6403f10702cf
VuLN <132083750+NTQD@users.noreply.github.com>
2025-03-31 00:20:26 +0700

    Add OverviewForm.Designer.cs 1743355244594

---
+++ OverviewForm.Designer.cs
@@ -0,0 +1,30 @@
+namespace FinanceApp
+{
+    partial class OverviewForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // OverviewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 300);
+            this.Name = "OverviewForm";
+            this.Text = "Financial Overview";
+            this.ResumeLayout(false);
+        }
+    }
+}
