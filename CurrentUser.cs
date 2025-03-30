5cff370c3a6b38ad8d05ef707da697af7825695e
VuLN <132083750+NTQD@users.noreply.github.com>
2025-03-30 22:24:57 +0700

    Add CurrentUser.cs 1743348315730

---
+++ CurrentUser.cs
@@ -0,0 +1,7 @@
+public static class CurrentUser
+{
+    public static int UserId { get; set; }
+    public static string Username { get; set; }
+    public static string Email { get; set; }
+    public static string FullName { get; set; }
+}
