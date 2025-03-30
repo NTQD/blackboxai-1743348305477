3a0214462bf4f7c48046eea6dae4f5f88f17ae42
VuLN <132083750+NTQD@users.noreply.github.com>
2025-03-31 00:20:47 +0700

    Add Models/IncomeCategory.cs 1743355265765

---
+++ Models/IncomeCategory.cs
@@ -0,0 +1,8 @@
+namespace FinanceApp.Models
+{
+    public class IncomeCategory
+    {
+        public int IncomeCategoryId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}
