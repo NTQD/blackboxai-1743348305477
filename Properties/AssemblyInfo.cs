87c13a67f0fd84d33b47cdc7316a7bcd2c628f05
VuLN <132083750+NTQD@users.noreply.github.com>
2025-03-31 22:58:41 +0700

    Add Properties/AssemblyInfo.cs 1743436740667

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FinanceApp")]
+[assembly: AssemblyDescription("Personal Finance Management Application")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FinanceApp")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("YOUR-GUID-HERE")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
